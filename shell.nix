083abfa79a0504bdee8f0b2248a8a990cfa8e369
dragonblade316 <dragonblade316@gmail.com>
2025-10-04 10:53:15 -0600

    init

---
+++ shell.nix
@@ -0,0 +1,14 @@
+# https://nix.dev/tutorials/declarative-and-reproducible-developer-environments
+with import <nixpkgs> { };
+
+mkShell {
+
+  # Package names can be found via https://search.nixos.org/packages
+  nativeBuildInputs = [
+    cmake
+    
+  ];
+
+  shellHook = ''
+  '';
+}
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use poise::serenity_prelude as serenity;
+// use songbird::SerenityInit;
+
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+
+    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            prefix_options: poise::PrefixFrameworkOptions { 
+                prefix: Some("!".into()), 
+                case_insensitive_commands: true,
+                ..Default::default()
+            },
+
+            commands: vec![age()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        // .register_songbird()
+        .await;
+    
+    client.unwrap().start().await.unwrap();
+}
